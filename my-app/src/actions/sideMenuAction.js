20c8edd8d20358b24707f274eb959a945330780f
DESKTOP-PEORPMR\kausar <kausar@zvky.com>
2021-03-30 07:56:33 +0530

    make it better

--- my-app/src/actions/sideMenuAction.js
+++
@@ -1,9 +0,0 @@
-import {GET_MENU_ITEMS} from './types';
-
-
-
-export const getMenuItems = () => {
-    return{
-        type : GET_MENU_ITEMS
-    }
-}
--- my-app/src/components/ProductManagement/ComponentToPrint.js
+++
@@ -1,9 +0,0 @@
-import React,  { Component } from 'react' ;
-import ProductTable from './ProductTable'
-class ComponentToPrint extends Component {
-  render() {
-    return <ProductTable ref={(el) => (this.componentRef = el)} />;
-    
-  }
-}
-export default ComponentToPrint;
